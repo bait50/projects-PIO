494340f251d29fc2bfec733a28d0eec5518cf31c
Dario <dario503030@gmail.com>
2024-10-09 17:03:28 -0500

    subiendo los avances del proyecto 2

---
+++ proyecto 2/main.py
@@ -0,0 +1,22 @@
+peli = [
+    {"nombre":"JOKER 2: FOLIE À DEUX"
+    ,"descricion":"Secuela de Joker (2019), de nuevo con Phoenix como Arthur Fleck, y que muestra su relación con el personaje de Harley Quinn, interpretado por Lady Gaga"
+    ,"genero":"Suspense, Drama, Cine dramático"
+    ,}
+]
+
+def cartelera(peli):
+    for pelis in peli:
+        print("---bienvenido a nuestra cartelera---")
+        print("")
+        print(f"nombre: {pelis['nombre']}")
+        print(f"descricion: {pelis['descricion']}")
+        print(f"genero: {pelis['genero']}")
+
+def actualizar_peli(peli):
+    print("---actualiza peli---")
+
+
+
+
+
