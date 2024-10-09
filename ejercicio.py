b944b70baab63dc8e929f5fbb7e2314047330f09
Dario <dario503030@gmail.com>
2024-10-09 14:30:46 -0500

    Elimino un archivo y agregando otro

---
+++ ejercicio.py
@@ -0,0 +1,12 @@
+# Ejercicio de concicionales
+def es_mayor_que_diez(numero):
+    # Tu lógica aquí
+    if numero > 10:
+        return True
+    else:
+        return False
+
+
+# Prueba la función con varios valores
+resultado = es_mayor_que_diez(1)  # Debería devolver True
+print(resultado)
